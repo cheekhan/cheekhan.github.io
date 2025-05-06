bf008a6b4216a73f3371abdc66459dd62f6ca986
CheeKhan <cheekhan@foxmail.com>
2025-05-06 09:22:28 +0800

    调整导航和六爻说明

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -12,6 +12,7 @@ export default defineConfig({
     nav: [
       { text: "首页", link: "/" },
       { text: "大六壬", link: "/personal/" },
+      { text: "六爻", link: "/liuyao/" },
       { text: "课例", link: "/LiurenCase/" },
       { text: "排盘", link: "/Bootstrap/" },
     ],
@@ -19,22 +20,30 @@ export default defineConfig({
     sidebar: {
       "/personal/": [
         {
+          text: "个人介绍",
+          link: "/personal/index",
+        },
+        {
           text: "六壬基础",
           items: [
-            { text: "我是哪个小饼干", link: "/personal/index" },
-            {
-              text: "大六壬",
-              items: [
-                { text: "前置概念", link: "/personal/MindReader/base" },
-                { text: "起课排传", link: "/personal/MindReader/setup" },
-                { text: "九宗门", link: "/personal/MindReader/九宗门" },
-                { text: "月将", link: "/personal/MindReader/月将" },
-                { text: "天将", link: "/personal/MindReader/十二天将" },
-                { text: "十二长生", link: "/personal/MindReader/十二长生" },
-                { text: "神煞", link: "/personal/MindReader/神煞" },
-              ],
-            },
+            { text: "前置概念", link: "/personal/MindReader/base" },
+            { text: "起课排传", link: "/personal/MindReader/setup" },
+            { text: "九宗门", link: "/personal/MindReader/九宗门" },
+            { text: "月将", link: "/personal/MindReader/月将" },
+            { text: "天将", link: "/personal/MindReader/十二天将" },
+            { text: "十二长生", link: "/personal/MindReader/十二长生" },
+            { text: "神煞", link: "/personal/MindReader/神煞" },
           ],
+        },
+      ],
+      "/liuyao/": [
+        {
+          text: "参考",
+          link: "/liuyao/index",
+        },
+        {
+          text: "占法",
+          link: "/liuyao/占法",
         },
       ],
       "/LiurenCase/": [
