daae2a372eb9bf0566af8cbde0e0dd028700f16b
CheeKhan <cheekhan@foxmail.com>
2025-11-09 15:16:24 +0800

    更新基础结构

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/theme-chalk/dark/css-vars.css'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(ElementPlus, {
+        locale: zhCn,
+    })
+    .mount('#app')
